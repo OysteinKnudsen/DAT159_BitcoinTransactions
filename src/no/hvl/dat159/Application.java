279aa5cb85a8bdf41c0dca0d6a7e26e7add05157
Ådne Siqveland <aadne8@gmail.com>
2018-10-23 13:12:54 +0200

    Final changes

--- src/no/hvl/dat159/Application.java
+++ src/no/hvl/dat159/Application.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Application {
     
     private static UTXO utxo = new UTXO();
+    private static int blockNumber = 0;
 	
 	public static void main(String[] args) throws Exception {
 	    
@@ -32,13 +33,13 @@ public class Application {
         // 1. The first "block" (= round of transactions) contains only a coinbase
         //    transaction. Create a coinbase transaction that adds a certain
         //    amount to the "miner"'s address. Update the UTXO-set (add only).
-        CoinbaseTx firstBlockCoinbase = new CoinbaseTx("genesis", 1, minerWallet.getAddress());
+        CoinbaseTx firstBlockCoinbase = new CoinbaseTx("genesis", 10, minerWallet.getAddress());
         utxo.addOutputFrom(firstBlockCoinbase);
 
         // 2. The second "block" contains two transactions, the mandatory coinbase
         //    transaction and a regular transaction. The regular transaction shall
         //    send ~20% of the money from the "miner"'s address to the other address.
-        performRegularTransaction(minerWallet, myWallet);
+        createRegularBlock(minerWallet, myWallet, "Hallo på do!");
 
 
         // 3. Do the same once more. Now, the "miner"'s address should have two or more
@@ -46,7 +47,7 @@ public class Application {
         //    total of 2.6 * block reward, and the other address should have 0.4 ...
         //    Validate the regular transaction ..
         //    Update the UTXO-set ...
-        performRegularTransaction(minerWallet, myWallet);
+        createRegularBlock(minerWallet, myWallet, "Hallo fra do!");
 
         // 4. Make a nice print-out of all that has happened, as well as the end status.
         //
@@ -62,20 +63,23 @@ public class Application {
         //      End status: the set of unspent outputs
         //      End status: for each of the wallets, print
         //          wallet id, address, balance
+        utxo.printUTXO();
+        System.out.println(myWallet.toString());
+        System.out.println(minerWallet.toString());
 	}
 
-    private static void performRegularTransaction(Wallet minerWallet, Wallet myWallet) throws Exception {
-        CoinbaseTx secondBlockCoinbase = new CoinbaseTx("hallo på do!", 1, minerWallet.getAddress());
-        utxo.addOutputFrom(secondBlockCoinbase);
-        Transaction secondBlockRegular = minerWallet.
+    private static void createRegularBlock(Wallet minerWallet, Wallet myWallet, String message) throws Exception {
+        CoinbaseTx coinbaseTx = new CoinbaseTx(message, 10, minerWallet.getAddress());
+        utxo.addOutputFrom(coinbaseTx);
+        Transaction regularTx = minerWallet.
                 createTransaction(Math.round(minerWallet.getBalance() * 0.2D), myWallet.getAddress());
 
         //    Validate the regular transaction created by the "miner"'s wallet:
-        if (!secondBlockRegular.isValid())
+        if (!regularTx.isValid())
             throw new Exception("Not valid block!");
 
         //      - All the inputs are unspent and belongs to the sender
-        for (Input input : secondBlockRegular.getInputs()) {
+        for (Input input : regularTx.getInputs()) {
             if (!utxo.getUTXOMap().containsKey(input))
                 throw new Exception("Not valid block!");
             if (!utxo.getUTXOMap().get(input).getAddress().equals(minerWallet.getAddress()))
@@ -84,25 +88,25 @@ public class Application {
 
         //      - There are no repeating inputs!!!
         List<String> prevTxHashes = new ArrayList<>();
-        for (Input input : secondBlockRegular.getInputs()) {
+        for (Input input : regularTx.getInputs()) {
             if (prevTxHashes.contains(input.getPrevTxHash()))
                 throw new Exception("Not valid block!");
             prevTxHashes.add(input.getPrevTxHash());
         }
 
         //      - All the outputs must have a value > 0
-        for (Output output : secondBlockRegular.getOutputs()) {
+        for (Output output : regularTx.getOutputs()) {
             if (output.getValue() <= 0)
                 throw new Exception("Not valid block!");
         }
 
         //      - The sum of inputs equals the sum of outputs
         long inputSum = 0;
-        for (Input input : secondBlockRegular.getInputs())
+        for (Input input : regularTx.getInputs())
             inputSum += utxo.getUTXOMap().get(input).getValue();
 
         long outputSum = 0;
-        for (Output output : secondBlockRegular.getOutputs())
+        for (Output output : regularTx.getOutputs())
             outputSum += output.getValue();
 
 
@@ -110,17 +114,22 @@ public class Application {
             throw new Exception("Not valid block!");
 
         //      - The transaction is correctly signed by the sender
-        if(DSAUtil.verifyWithDSA(minerWallet.getPublicKey(), secondBlockRegular.getMessage(), secondBlockRegular.getSignature())) {
+        if(!DSAUtil.verifyWithDSA(minerWallet.getPublicKey(), regularTx.getMessage(), regularTx.getSignature()))
             throw new Exception("Not valid block!");
-        }
+
 
         //      - The transaction hash is correct
-        String newTxHash = HashUtil.base64Encode(HashUtil.sha256Hash(secondBlockRegular.getMessage()));
-        if (secondBlockRegular.getTxHash().equals(newTxHash))
+        String newTxHash = HashUtil.base64Encode(HashUtil.sha256Hash(regularTx.getMessage()));
+        if (!regularTx.getTxHash().equals(newTxHash))
             throw new Exception("Not valid block!");
 
-
         //    Update the UTXO-set (both add and remove).
-        utxo.addAndRemoveOutputsFrom(secondBlockRegular);
+        utxo.addAndRemoveOutputsFrom(regularTx);
+
+        System.out.println("Block number " + blockNumber++ + ":");
+        System.out.println(coinbaseTx.toString());
+        System.out.println(regularTx.toString());
+        System.out.println();
+
     }
 }
